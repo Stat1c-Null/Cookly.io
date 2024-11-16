3391f1f748532b4c1bc091fe26aff27919c02e65
Mikita Slabysh <slabysh2015@gmail.com>
2024-11-15 19:17:50 -0500

    Working on integrating gemini image recognition

--- food_recognition.py
+++ food_recognition.py
@@ -1,16 +1,34 @@
-import cv2
-import deeplake
+import cv2, deeplake
 import numpy as np
-import hub
+import pathlib
+import textwrap
+import os
+
+import google.generativeai as genai
+
+from IPython.display import display
+from IPython.display import Markdown
+
+
+GEMINI_KEY = os.environ['API_KEY']
+
+def to_markdown(text):
+  text = text.replace('•', '  *')
+  return Markdown(textwrap.indent(text, '> ', predicate=lambda _: True))
+
+genai.configure(api_key=GEMINI_KEY)
+
+model = genai.GenerativeModel('gemini-1.5-flash')
+
 #https://www.kaggle.com/code/sainikhileshreddy/how-to-use-the-dataset
 #Trained model to detect food
-try:
+"""try:
   #food_model = deeplake.load('hub://sainikhileshreddy/food-recognition-2022-test')
   food_model = hub.load('/kaggle/input/food-recognition-2022/hub/val')
   print("Succesfully loaded Dataset")
 except Exception as e:
   print(f"Dataset couldnt be loaded {e}")
-  exit()
+  exit()"""
 
 #Load image
 img_path = 'images/potatoes.png'
@@ -24,10 +42,9 @@ resized_img = cv2.resize(img, (224, 224))
 resized_img = resized_img / 255.0
 resized_img = np.expand_dims(resized_img, axis=0)
 
-try:
-    print(food_model.labels)
-except Exception as e:
-    print(f"Error during inference: {e}")
+response = model.generate_content(["Tell me what food is in the picture", resized_img], stream=True)
+
+response.resolve()
 
 cv2.imshow('Food Image', img)
 cv2.waitKey(0)
