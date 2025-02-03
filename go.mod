6b4b4d778f824ba1d1bff3c34bab8d1aab72430d
Chris <jedisquirrel86@gmail.com>
2025-02-02 20:04:54 -0500

    hello-world webapp compiles - doesn't work yet

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/Stat1c-Null/Cookly.io
 
 go 1.23.5
+
+require (
+	github.com/gorilla/securecookie v1.1.2 // indirect
+	github.com/gorilla/sessions v1.4.0 // indirect
+	github.com/mattn/go-sqlite3 v1.14.24 // indirect
+)
--- main.go
+++ main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"embed"
+	"fmt"
+	"log"
+	"net/http"
+	"github.com/Stat1c-Null/Cookly.io/db"
+	"github.com/Stat1c-Null/Cookly.io/handlers"
+	"github.com/Stat1c-Null/Cookly.io/templates"
+)
+
+//go:embed static/*
+var staticFiles embed.FS
 
 func main() {
-	fmt.Print("Hello world")
+	// Initialize database
+	db.InitDB()
+
+	// Initialize templates
+	templates.Init()
+
+	// Serve static files
+	http.Handle("/static/", http.FileServer(http.FS(staticFiles)))
+
+	// Routes
+	http.HandleFunc("/", handlers.HomeHandler)
+	http.HandleFunc("/login", handlers.LoginHandler)
+	http.HandleFunc("/register", handlers.RegisterHandler)
+	http.HandleFunc("/logout", handlers.LogoutHandler)
+
+	fmt.Println("Server running on :8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
